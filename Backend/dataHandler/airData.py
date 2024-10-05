8986cd0d9cbf26b8eac774a26c00044d5fe135a0
rytlebsk <111889013+rytlebsk@users.noreply.github.com>
2024-10-05 12:15:34 +0800

    try to solve no data's problem

--- Backend/dataHandler/airData.py
+++ Backend/dataHandler/airData.py
@@ -1,8 +1,8 @@
 from datetime import datetime
-import requests
+import requests,pytz
 
 def getAirData(lon,lat): 
-    nowTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")#取得符合取得資料格式的時間
+    nowTime = datetime.now(pytz.timezone('Asia/Taipei')).strftime("%Y-%m-%d %H:%M:%S")#取得符合取得資料格式的時間
     url = f"https://data.moenv.gov.tw/api/v2/aqx_p_432?&api_key=7fcc7250-55c6-497a-aa44-cfba6d8f2c83&filters=publishtime,LE,{nowTime}&fields=sitename,aqi,pm2.5,longitude,latitude"
     try:
         AQIData = requests.get(url, verify=False).json()["records"]# 把憑證改回來
@@ -24,13 +24,17 @@ def getAirData(lon,lat):
                 "sitename": None,
                 "aqi":      None,
                 "pm2.5":    None,
-                "error":    "siteName no data"
+                "error":    "siteName no data",
+                "dis":      distant,
+                "time":     nowTime
             }
     except Exception as e :
             return {
                 "sitename": None,
                 "aqi":      None,
                 "pm2.5":    None,
-                "error":    e
+                "error":    e,
+                "dis":      distant,
+                "time":     nowTime
             }
     
