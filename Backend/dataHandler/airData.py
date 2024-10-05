2a13a60dc1f4eef841ac6468a0b1adcdb4e67d55
rytlebsk <111889013+rytlebsk@users.noreply.github.com>
2024-10-05 11:29:55 +0800

    add catch error

--- Backend/dataHandler/airData.py
+++ Backend/dataHandler/airData.py
@@ -1,19 +1,36 @@
 from datetime import datetime
-import requests,math
+import requests
 
 def getAirData(lon,lat): 
     nowTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")#取得符合取得資料格式的時間
     url = f"https://data.moenv.gov.tw/api/v2/aqx_p_432?&api_key=7fcc7250-55c6-497a-aa44-cfba6d8f2c83&filters=publishtime,LE,{nowTime}&fields=sitename,aqi,pm2.5,longitude,latitude"
-    AQIData = requests.get(url, verify=False).json()["records"]# 把憑證改回來
-    distant = float(100)
-    siteName = None
-    for site in AQIData:
-        distant_temp = (((float(site["longitude"])-lon)**2)+(((float(site["latitude"])-lat)**2)))**0.5
-        if distant_temp < distant :
-            distant = distant_temp
-            siteName = site
-    return {
-        "sitename": siteName["sitename"],
-        "aqi":      siteName["aqi"],
-        "pm2.5":    siteName["pm2.5"]
-    }
+    try:
+        AQIData = requests.get(url, verify=False).json()["records"]# 把憑證改回來
+        distant = float(100)
+        siteName = None
+        for site in AQIData:
+            distant_temp = (((float(site["longitude"])-lon)**2)+(((float(site["latitude"])-lat)**2)))**0.5
+            if distant_temp < distant :
+                distant = distant_temp
+                siteName = site
+        if(siteName):
+            return {
+                "sitename": siteName["sitename"],
+                "aqi":      siteName["aqi"],
+                "pm2.5":    siteName["pm2.5"]
+            }
+        else:
+            return {
+                "sitename": None,
+                "aqi":      None,
+                "pm2.5":    None,
+                "error":    "siteName no data"
+            }
+    except Exception as e :
+            return {
+                "sitename": None,
+                "aqi":      None,
+                "pm2.5":    None,
+                "error":    e
+            }
+    
