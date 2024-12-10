47e3d12e1af81500c398c243ddaa5055e43e3eec
rytlebsk <111889013+rytlebsk@users.noreply.github.com>
2024-12-10 15:30:19 +0800

    update cwa new format

--- Backend/dataHandler/weatherData.py
+++ Backend/dataHandler/weatherData.py
@@ -1,6 +1,5 @@
 from datetime import datetime, timedelta
 from .airData import getAirData
-from .methodPack import setLocate
 from .locationToAxis import transToAxis
 from datetime import datetime
 import requests
@@ -37,13 +36,13 @@ def getTime(t): return (t - timedelta(minutes=90)
 
 def city():
     citycode = None
-    with open('cityCode.json') as f:
+    with open('/Users/weiyouen/Documents/GitHub/Weather_Forecast_App_formatChangeVer./Backend/cityCode.json') as f:
         citycode = json.load(f)
     return citycode
 
 
 def url(ft, nowCity, nowdistrict, nowTime):
-    return f"https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-D0047-0{city()[nowCity+ft]}?Authorization=CWA-3D385D45-EFD5-4BD3-9677-9100AD39A4A2&LocationName={nowdistrict}&ElementName=T,Wx,RH,WS,WD,AT,MaxAT,MinAT,{'PoP6h'if ft=='3h'else'PoP12h'},WeatherDescription&sort=time&timeFrom={nowTime}"
+    return f"https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-D0047-0{city()[nowCity+ft]}?Authorization=CWA-3D385D45-EFD5-4BD3-9677-9100AD39A4A2&locationName={nowdistrict}&sort=time&timeFrom={nowTime}"
 
 
 def get3hData(cusloc):
@@ -62,24 +61,23 @@ def get3hData(cusloc):
         print("Redis connection failed, retrieving fresh data.")
 
     weatherData = requests.get(url('3h', loc["city"], loc["district"], offsetTime)).json()[
-        "records"]["locations"][0]["location"][0]["weatherElement"]
+        "records"]["Locations"][0]["Location"][0]["WeatherElement"]
     resultElement = []  # 初始化陣列
-    noData = 1 if datetime.strptime(
-        weatherData[0]["time"][0]["startTime"], "%Y-%m-%d %H:%M:%S").hour % 6 != 0 else 0
+    timeOffset = 3-datetime.strptime(weatherData[0]["Time"][0]["DataTime"],"%Y-%m-%d %H:%M:%S").hour % 3
     airData = getAirData(axis["lon"], axis["lat"])
 
-    for time in range(len(weatherData[0]["time"])):
+    for time in range(len(weatherData[8]["Time"])):
         resultElement.append({**{
-            "weatherText": weatherData[0]["time"][time]["elementValue"][0]["value"],
-            "weatherCode": weatherData[0]["time"][time]["elementValue"][1]["value"],
-            "bodyTemp": weatherData[1]["time"][time]["elementValue"][0]["value"],
-            "temp": weatherData[2]["time"][time]["elementValue"][0]["value"],
-            "wet": weatherData[3]["time"][time]["elementValue"][0]["value"],
-            "weatherDes": weatherData[4]["time"][time]["elementValue"][0]["value"],
-            "rainRate": weatherData[5]["time"][int((time - noData) / 2)]["elementValue"][0]["value"],
-            "windSpeed": weatherData[6]["time"][time]["elementValue"][0]["value"],
-            "windDirection": weatherData[7]["time"][time]["elementValue"][0]["value"],
-            "time": weatherData[0]["time"][time]["startTime"],
+            "weatherText": weatherData[8]["Time"][time]["ElementValue"][0]["Weather"],
+            "weatherCode": weatherData[8]["Time"][time]["ElementValue"][0]["WeatherCode"],
+            "bodyTemp": weatherData[3]["Time"][time+timeOffset]["ElementValue"][0]["ApparentTemperature"],
+            "temp": weatherData[0]["Time"][time+timeOffset]["ElementValue"][0]["Temperature"],
+            "wet": weatherData[2]["Time"][time+timeOffset]["ElementValue"][0]["RelativeHumidity"],
+            "weatherDes": weatherData[9]["Time"][time]["ElementValue"][0]["WeatherDescription"],
+            "rainRate": weatherData[7]["Time"][time]["ElementValue"][0]["ProbabilityOfPrecipitation"],
+            "windSpeed": weatherData[5]["Time"][time]["ElementValue"][0]["WindSpeed"],
+            "windDirection": weatherData[6]["Time"][time]["ElementValue"][0]["WindDirection"],
+            "time": weatherData[8]["Time"][time]["StartTime"],
             "city": loc["city"],
             "district": loc["district"]
         }, **(airData if time == 0 else {
@@ -93,9 +91,7 @@ def get3hData(cusloc):
         r_write.setex(cache_key, 3600, json.dumps(resultElement))
     except ConnectionError:
         print("Redis unavailable for write, skip caching.")
-
     return resultElement
-
 
 def get12hData(cusloc):
     loc = cusloc  # 引入地理編碼
@@ -111,27 +107,27 @@ def get12hData(cusloc):
     except Exception as e:
         print("Issue with Redis (read), skip caching.:", e)
     weatherData = requests.get(url('12h', loc["city"], loc["district"], offsetTime)).json()[
-        "records"]["locations"][0]["location"][0]["weatherElement"]
+        "records"]["Locations"][0]["Location"][0]["WeatherElement"]
     resultElement = []  # 初始化陣列
     dayOffset = 0 if datetime.strptime(
-        weatherData[0]["time"][0]["startTime"], "%Y-%m-%d %H:%M:%S").hour < 18 else 1  # 調整數據為白天
+        weatherData[0]["Time"][0]["StartTime"], "%Y-%m-%d %H:%M:%S").hour < 18 else 1  # 調整數據為白天
     if dayOffset:
         resultElement.append(get3hData(cusloc)[0])
 
-    for time in range(0, len(weatherData[0]["time"])-dayOffset, 2):
-        rainRateDataNow = weatherData[0]["time"][time +
-                                                 dayOffset]["elementValue"][0]["value"]
+    for time in range(0, len(weatherData[0]["Time"])-dayOffset, 2):
+        rainRateDataNow = weatherData[11]["Time"][time +
+                                                 dayOffset]["ElementValue"][0]["ProbabilityOfPrecipitation"]
         resultElement.append({
-            "weatherText":   weatherData[5]["time"][time+dayOffset]["elementValue"][0]["value"],
-            "weatherCode":   weatherData[5]["time"][time+dayOffset]["elementValue"][1]["value"],
-            "bodyTemp":      str(math.ceil((int(weatherData[4]["time"][time+dayOffset]["elementValue"][0]["value"]) + int(weatherData[7]["time"][time+dayOffset]["elementValue"][0]["value"]))/2)),
-            "temp":          weatherData[1]["time"][time+dayOffset]["elementValue"][0]["value"],
-            "wet":           weatherData[2]["time"][time+dayOffset]["elementValue"][0]["value"],
-            "weatherDes":    weatherData[6]["time"][time+dayOffset]["elementValue"][0]["value"],
-            "rainRate":      rainRateDataNow if rainRateDataNow != " " else "尚無資料",
-            "windSpeed":     weatherData[4]["time"][time+dayOffset]["elementValue"][0]["value"],
-            "windDirection": weatherData[8]["time"][time+dayOffset]["elementValue"][0]["value"],
-            "time":          weatherData[0]["time"][time+dayOffset]["startTime"],
+            "weatherText":   weatherData[12]["Time"][time+dayOffset]["ElementValue"][0]["Weather"],
+            "weatherCode":   weatherData[12]["Time"][time+dayOffset]["ElementValue"][0]["WeatherCode"],
+            "bodyTemp":      str(math.ceil((int(weatherData[5]["Time"][time+dayOffset]["ElementValue"][0]["MaxApparentTemperature"]) + int(weatherData[6]["Time"][time+dayOffset]["ElementValue"][0]["MinApparentTemperature"]))/2)),
+            "temp":          weatherData[0]["Time"][time+dayOffset]["ElementValue"][0]["Temperature"],
+            "wet":           weatherData[4]["Time"][time+dayOffset]["ElementValue"][0]["RelativeHumidity"],
+            "weatherDes":    weatherData[14]["Time"][time+dayOffset]["ElementValue"][0]["WeatherDescription"],
+            "rainRate":      rainRateDataNow if rainRateDataNow != "-" else "尚無資料",
+            "windSpeed":     weatherData[9]["Time"][time+dayOffset]["ElementValue"][0]["WindSpeed"],
+            "windDirection": weatherData[10]["Time"][time+dayOffset]["ElementValue"][0]["WindDirection"],
+            "time":          weatherData[0]["Time"][time+dayOffset]["StartTime"],
             "city":          loc["city"],
             "district":      loc["district"],
             "sitename": None,
@@ -144,8 +140,6 @@ def get12hData(cusloc):
     except Exception as e:
         print("Issue with Redis (write), skip caching.:", e)
     return resultElement
-
-
 
 # 121.66248756678424121,25.06715187342581
 # 已知問題
